98a189d3281479043e3e84d83164177682184f7c
irosgrim <irosgrim@gmail.com>
2019-02-26 08:35:44 +0100

    reactJs note taker app

---
+++ notes/precache-manifest.9b3ae3f621136dbe2f21ab2a5c10b1ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "694724f536426d5f01a2",
+    "url": "/static/js/main.694724f5.chunk.js"
+  },
+  {
+    "revision": "b59443390daaa51ac094",
+    "url": "/static/js/2.b5944339.chunk.js"
+  },
+  {
+    "revision": "694724f536426d5f01a2",
+    "url": "/static/css/main.817dc5fc.chunk.css"
+  },
+  {
+    "revision": "ebf305c358b784e7616e6a1ee29a2d12",
+    "url": "/index.html"
+  }
+];
