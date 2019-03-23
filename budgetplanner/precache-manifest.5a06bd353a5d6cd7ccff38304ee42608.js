c2f48fb25483ee8822874d2d7e0682a1cc95940a
irosgrim <irosgrim@gmail.com>
2019-03-23 12:51:59 +0100

    ReactJs budget planner app

---
+++ budgetplanner/precache-manifest.5a06bd353a5d6cd7ccff38304ee42608.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "7bf243e49a5698e210d4",
+    "url": "./static/js/main.3669c147.chunk.js"
+  },
+  {
+    "revision": "9973e487ba3df70e8df8",
+    "url": "./static/js/2.68408ec7.chunk.js"
+  },
+  {
+    "revision": "7bf243e49a5698e210d4",
+    "url": "./static/css/main.19990723.chunk.css"
+  },
+  {
+    "revision": "85187ee26e1efefe42be33436397cfe7",
+    "url": "./index.html"
+  }
+];
--- budgetplanner/service-worker.js
+++ budgetplanner/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c0ad1b27cc13513f2b53f49c9d42f3ea.js"
+  "./precache-manifest.5a06bd353a5d6cd7ccff38304ee42608.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
