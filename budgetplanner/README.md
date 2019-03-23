20845837e5eacbf02221d8e1136af64ccb7d6116
irosgrim <irosgrim@gmail.com>
2019-03-23 12:46:59 +0100

    ReactJs budget planner app

---
+++ budgetplanner/README.md
@@ -0,0 +1,15 @@
+# ReactJs - Budget planner, based on the Donut Chart component i built
+
+![donut chart](budgetplanner.png)
+
+## Available Scripts
+
+### `npm start`
+
+### `npm test`
+
+### `npm run build`
+
+```
+
+```
---
+++ budgetplanner/precache-manifest.c0ad1b27cc13513f2b53f49c9d42f3ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e79ef827893f5f8aef07",
+    "url": "/static/js/main.dbdbacef.chunk.js"
+  },
+  {
+    "revision": "9973e487ba3df70e8df8",
+    "url": "/static/js/2.68408ec7.chunk.js"
+  },
+  {
+    "revision": "e79ef827893f5f8aef07",
+    "url": "/static/css/main.19990723.chunk.css"
+  },
+  {
+    "revision": "1e40c20ad3d8075703e761e73e21a4da",
+    "url": "/index.html"
+  }
+];
